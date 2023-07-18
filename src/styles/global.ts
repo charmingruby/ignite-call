b4042d65ea41bce4d200b9cfd54d0408ae15ee31
charmingruby <mrrubycanal3105@gmail.com>
2023-07-18 19:47:07 -0300

    setup

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,3 +1,15 @@
 import { globalCss } from '@ignite-ui/react'
 
-export const globalStles = globalCss
+export const globalStyles = globalCss({
+  '*': {
+    boxSizing: 'border-box',
+    padding: 0,
+    margin: 0,
+  },
+
+  body: {
+    backgroundColor: '$gray900',
+    color: '$gray100',
+    '-webkit-font-smoothing': 'antialiased',
+  },
+})
