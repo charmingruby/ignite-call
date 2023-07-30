3541e966b2499bbdb35995d001fbdd4a3662ed13
charmingruby <mrrubycanal3105@gmail.com>
2023-07-30 13:23:28 -0300

    google o auth config

--- src/pages/register/connect-calendar/index.page.tsx
+++ src/pages/register/connect-calendar/index.page.tsx
@@ -2,10 +2,9 @@ import { Button, Heading, MultiStep, Text } from '@ignite-ui/react'
 import { Container, Header } from '../styles'
 import { ArrowRight } from 'phosphor-react'
 import { ConnectBox, ConnectItem } from './styles'
+import { signIn } from 'next-auth/react'
 
 export default function ConnectCalendar() {
-  //  async function handleRegister(data: RegisterFormData) {}
-
   return (
     <Container>
       <Header>
@@ -21,7 +20,13 @@ export default function ConnectCalendar() {
       <ConnectBox>
         <ConnectItem>
           <Text>Google Calendar</Text>
-          <Button variant="secondary" size="sm">
+          <Button
+            variant="secondary"
+            size="sm"
+            onClick={() => {
+              signIn('google')
+            }}
+          >
             Conectar
             <ArrowRight />
           </Button>
